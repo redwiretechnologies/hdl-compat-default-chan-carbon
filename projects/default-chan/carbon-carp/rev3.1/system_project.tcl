ba4a319f3cbe566b54d8bdb5685b3c8dd71bbd99
Jonathan Ambrose <jambrose@redwiretechnologies.us>
2024-04-17 16:07:15 -0400

    Removing old board revisions not available for public release

--- projects/default-chan/carbon-carp/rev3.1/system_project.tcl
+++
@@ -1,2 +0,0 @@
-set script_dir [ file dirname [ file normalize [ info script ] ] ]
-source "$script_dir/../../../common/scripts/common_project.tcl"
--- projects/default-chan/carbon-carp/rev3.1/vivado_directives.tcl
+++
@@ -1,7 +0,0 @@
-set full_board [get_parts -of_objects [get_projects]]
-set board [string range $full_board 4 7]
-
-if { ( $board == "15eg" ) } {
-    set_property STEPS.OPT_DESIGN.ARGS.DIRECTIVE ExploreWithRemap [get_runs impl_1]
-    set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE ExtraTimingOpt [get_runs impl_1]
-}
