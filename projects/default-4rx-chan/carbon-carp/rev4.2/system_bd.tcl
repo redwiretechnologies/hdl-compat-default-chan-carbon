faba9d5389bc49fd2320d0c6aeb2c0026e15fc12
Jonathan Ambrose <jambrose@redwiretechnologies.us>
2024-08-12 11:02:53 -0400

    Adding support for CARP rev2

---
+++ projects/default-4rx-chan/carbon-carp/rev4.2/system_bd.tcl
@@ -0,0 +1,10 @@
+set script_dir [file dirname [ file dirname [ file normalize [ info script ]/... ] ] ]
+
+puts $script_dir
+
+source "$script_dir/../../../common/carbon-carp/rev4.2/carbon-carp_system_bd.tcl"
+source "$script_dir/../../scripts/default-4rx-chan_bd.tcl"
+source "$script_dir/../../../common/scripts/common_bd.tcl"
+
+ad_ip_parameter axi_ad9361_0 CONFIG.ADC_INIT_DELAY 11
+ad_ip_parameter axi_ad9361_1 CONFIG.ADC_INIT_DELAY 11
---
+++ projects/default-4rx-chan/carbon-carp/rev4.2/vivado_directives.tcl
@@ -0,0 +1,23 @@
+set full_board [get_parts -of_objects [get_projects]]
+set board [string range $full_board 4 6]
+
+if { ( $board == "9eg" || $board == "4ev" ) } {
+    set_property STEPS.OPT_DESIGN.ARGS.DIRECTIVE ExploreWithRemap [get_runs impl_1]
+    set_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE ExploreWithAggressiveHoldFix [get_runs impl_1]
+
+    set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE ExtraTimingOpt [get_runs impl_1]
+}
+
+if { ( $board == "9eg" || $board == "3eg" ) } {
+    set_property STEPS.OPT_DESIGN.ARGS.DIRECTIVE ExploreWithRemap [get_runs impl_1]
+    set_property STEPS.ROUTE_DESIGN.ARGS.DIRECTIVE NoTimingRelaxation [get_runs impl_1]
+    set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
+}
+
+if { ( $board == "9eg" ) } {
+    set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE SSI_HighUtilSLRs [get_runs impl_1]
+}
+
+if { ( $board == "15e" ) } {
+    set_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE ExploreWithAggressiveHoldFix [get_runs impl_1]
+}
