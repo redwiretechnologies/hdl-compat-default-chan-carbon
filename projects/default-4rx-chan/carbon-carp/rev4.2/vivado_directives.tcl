0471c2cf688d36bb3130b1536e4815fc85fe8b16
Jonathan Ambrose <jambrose@redwiretechnologies.us>
2025-12-09 10:00:43 -0500

    Updates to meet timing

--- projects/default-4rx-chan/carbon-carp/rev4.2/vivado_directives.tcl
+++ projects/default-4rx-chan/carbon-carp/rev4.2/vivado_directives.tcl
@@ -16,10 +16,11 @@ if { ( $board == "9eg" || $board == "3eg" ) } {
     set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
 }
 
-if { ( $board == "9eg" ) } {
-    set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE SSI_HighUtilSLRs [get_runs impl_1]
-}
-
-if { ( $board == "15e" ) } {
-    set_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE ExploreWithAggressiveHoldFix [get_runs impl_1]
+if { ( $board == "9eg" || $board == "15e" ) } {
+    set_property STEPS.OPT_DESIGN.ARGS.DIRECTIVE ExploreArea [get_runs impl_1]
+    set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE SSI_SpreadLogic_low [get_runs impl_1]
+    set_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE AggressiveFanoutOpt [get_runs impl_1]
+    set_property STEPS.ROUTE_DESIGN.ARGS.DIRECTIVE NoTimingRelaxation [get_runs impl_1]
+    set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
+    set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.ARGS.DIRECTIVE AggressiveExplore [get_runs impl_1]
 }
